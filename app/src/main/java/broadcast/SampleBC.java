86fb5feccd52b00e5d23f779b991ca789977213e
missmetal <mmartinez@est.frba.utn.edu.ar>
2016-12-23 18:00:03 -0300

    Primer commit
    Pendiente acoodar querys

---
+++ app/src/main/java/broadcast/SampleBC.java
@@ -0,0 +1,74 @@
+package broadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestHandle;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+import services.DBService;
+
+/**
+ * Created by Miss Metal on 27/11/2016.
+ */
+
+public class SampleBC extends BroadcastReceiver {
+    static int noOfTimes = 0;
+
+    // Method gets called when Broad Case is issued from MainActivity for every 10 seconds
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        // TODO Auto-generated method stub
+        noOfTimes++;
+        Toast.makeText(context, "BC Service Running for " + noOfTimes + " times", Toast.LENGTH_SHORT).show();
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        // Checks if new records are inserted in Remote MySQL DB to proceed with Sync operation
+        RequestHandle post = client.post("http://192.168.2.4:9000/mysqlsqlitesync/getdbrowcount.php", params, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                System.out.println(responseBody);
+                try {
+                    // Create JSON object out of the response sent by getdbrowcount.php
+                    JSONObject obj = new JSONObject(String.valueOf(responseBody));
+                    System.out.println(obj.get("count"));
+                    // If the count value is not zero, call MyService to display notification
+                    if (obj.getInt("count") != 0) {
+                        final Intent intnt = new Intent(context, DBService.class);
+                        // Set unsynced count in intent data
+                        intnt.putExtra("intntdata", "Unsynced Rows Count " + obj.getInt("count"));
+                        // Call MyService
+                        context.startService(intnt);
+                    } else {
+                        Toast.makeText(context, "Sync not needed", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                // TODO Auto-generated method stub
+                if (statusCode == 404) {
+                    Toast.makeText(context, "404", Toast.LENGTH_SHORT).show();
+                } else if (statusCode == 500) {
+                    Toast.makeText(context, "500", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(context, "Error occured!", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+        });
+    }
+}
---
+++ app/src/main/java/services/DBService.java
@@ -0,0 +1,75 @@
+package services;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.widget.Toast;
+
+import com.missmetal.dbsync.MainActivity;
+import com.missmetal.dbsync.R;
+
+/**
+ * Created by Miss Metal on 27/11/2016.
+ */
+
+public class DBService extends Service {
+    int numMessages = 0;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        throw new UnsupportedOperationException("No implementado");
+    }
+
+    @Override
+    public void onCreate() {
+
+        Toast.makeText(this, "DBService creado", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(this, "Servicio iniciado", Toast.LENGTH_LONG).show();
+        Intent resultIntent = new Intent(this, MainActivity.class);
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(this, 0,
+                resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder mNotifyBuilder;
+        NotificationManager mNotificationManager;
+        mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        // Sets an ID for the notification, so it can be updated
+        int notifyID = 9001;
+        mNotifyBuilder = new NotificationCompat.Builder(this)
+                .setContentTitle("Alert")
+                .setContentText("You've received new messages.")
+                .setSmallIcon(R.drawable.ic_launcher);
+        // Set pending intent
+        mNotifyBuilder.setContentIntent(resultPendingIntent);
+        // Set Vibrate, Sound and Light
+        int defaults = 0;
+        defaults = defaults | Notification.DEFAULT_LIGHTS;
+        defaults = defaults | Notification.DEFAULT_VIBRATE;
+        defaults = defaults | Notification.DEFAULT_SOUND;
+        mNotifyBuilder.setDefaults(defaults);
+        // Set the content for Notification
+        mNotifyBuilder.setContentText(intent.getStringExtra("intntdata"));
+        // Set autocancel
+        mNotifyBuilder.setAutoCancel(true);
+        // Post a notification
+        mNotificationManager.notify(notifyID, mNotifyBuilder.build());
+        return START_NOT_STICKY;
+
+
+    }
+
+    @Override
+    public void onDestroy() {
+        Toast.makeText(this, "Service Destroyed", Toast.LENGTH_LONG).show();
+
+    }
+}
